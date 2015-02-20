c0a697ad763f0582f7209a838d3543741bece631
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-20 12:33:26 -0500

    Added 14 of the gods, I'll add the last 11 later

--- gods.js
+++ gods.js
@@ -2,5 +2,20 @@
 // The array filter function 
 // Started 2/6/15
 
-var godList = [];
+var godList = [
+'{ "name": "agni", "pantheon": "hindu", "class": "mage", "attack type": "ranged", "power type": "magical" }',
+'{ "name": "anhur", "pantheon": "egyptian", "class": "hunter", "attack type": "ranged", "power type": "physical" }',
+'{ "name": "anubis", "pantheon": "egyptian", "class": "mage", "attack type": "ranged", "power type": "magical" }',
+'{ "name": "apollo", "pantheon": "greek", "class": "hunter", "attack type": "ranged", "power type": "physical" }',
+'{ "name": "ares", "pantheon": "greek", "class": "guardian", "attack type": melee", "power type": "magical" }',
+'{ "name: "awilix", "pantheon": "mayan", "class": "assassin", "attack type": melee", "power type": "physical" }',
+'{ "name: "bellona", "pantheon": "roman", "class": "warrior", "attack type": melee", "power type": "physical" }',
+'{ "name": "chaac", "pantheon": "mayan", "class": "warrior", "attack type": melee", "power type": "physical" }',
+'{ "name": "chronos", "pantheon": "greek", "class": "mage", "attack type": ranged", "power type": "magical" }',
+'{ "name": "cupid", "pantheon": "roman", "class": "hunter", "attack type": ranged", "power type": "physical" }',
+'{ "name": "fenrir", "pantheon": "norse", "class": "assassin", "attack type": "melee", "power type": "physical" }',
+'{ "name": "hades", "pantheon": "greek", "class": "guardian", "attack type": "ranged", "power type": "magical" }',
+'{ "name": "hel", "pantheon": "norse", "class": "mage", "attack type": "ranged", "power type": "magical" }',
+'{ "name": "hercules", "pantheon": "roman", "class": "warrior", "attack type": "melee", "power type": "physical" }',
+];
 console.log(godList);
